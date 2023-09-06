da92511aa44c333ff99db59211e688d41f2abf91
omkar1713 <56976982+omkar1713@users.noreply.github.com>
2023-09-06 13:01:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.title('Hello streamlit')
 streamlit.header('Hello regular streamlit')
 streamlit.text('Hello small streamlit')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
