890ea95131ba678182a1164866257cacdca592c1
omkar1713 <56976982+omkar1713@users.noreply.github.com>
2023-09-06 13:07:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ streamlit.title('Hello streamlit')
 streamlit.header('Hello regular streamlit')
 streamlit.text('Hello small streamlit')
 
+streamlit.title('Build your own fruit smoothie')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
